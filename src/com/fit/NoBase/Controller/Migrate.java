84eab11d385daab7e80c1d1bf94818c094627604
nartoan <toan.td.uet@gmail.com>
2017-05-10 04:20:55 +0700

    Migrate 2 tables

--- src/com/fit/NoBase/Controller/Migrate.java
+++ src/com/fit/NoBase/Controller/Migrate.java
@@ -3,14 +3,10 @@ package com.fit.NoBase.Controller;
 import com.fit.NoBase.Model.MySQLConnect;
 import com.fit.NoBase.Model.OracleNoSQLConnect;
 import oracle.kv.KVStore;
-import oracle.kv.StatementResult;
-import oracle.kv.impl.api.table.ArrayBuilder;
-import oracle.kv.impl.api.table.ArrayDefImpl;
 import oracle.kv.table.*;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 /**
  * Created by nartoan on 10/05/2017.
@@ -35,7 +31,7 @@ public class Migrate {
              * Query trong mysql
              */
             Statement statementSQL = connectSQL.createStatement();
-            String sql = "SELECT * FROM title LIMIT 10";
+            String sql = "SELECT id FROM title";
             ResultSet resultSet = statementSQL.executeQuery(sql);
 
             /**
@@ -52,33 +48,36 @@ public class Migrate {
                 /**
                  * Insert data into table title
                  */
-                int col = resultSetMetaData.getColumnCount();
-                for (int j = 1; j <= col; j++) {
-                    if (resultSet.getString(j) != null) {
-                        if (j != 4) {
-                            int type = resultSetMetaData.getColumnType(j);
-                            if (type == Types.INTEGER) {
-                                row.put(resultSetMetaData.getColumnName(j), resultSet.getInt(j));
-                            } else {
-                                row.put(resultSetMetaData.getColumnName(j), resultSet.getString(j));
-                            }
-                        } else {
-                            Statement statementSQL2 = connectSQL.createStatement();
-                            ResultSet resultSet2 = statementSQL2.executeQuery("SELECT * FROM kind_type");
-                            resultSet2.next();
+                int index = resultSet.getInt(1);
+                System.out.println(dem++ + ".......");
+//                insertInto_complete_cast(index, tableAPI);
+ //               int col = resultSetMetaData.getColumnCount();
+//                for (int j = 1; j <= col; j++) {
+//                    if (resultSet.getString(j) != null) {
+//                        if (j != 4) {
+//                            int type = resultSetMetaData.getColumnType(j);
+//                            if (type == Types.INTEGER) {
+//                                row.put(resultSetMetaData.getColumnName(j), resultSet.getInt(j));
+//                            } else {
+//                                row.put(resultSetMetaData.getColumnName(j), resultSet.getString(j));
+//                            }
+//                        } else {
+//                            Statement statementSQL2 = connectSQL.createStatement();
+//                            ResultSet resultSet2 = statementSQL2.executeQuery("SELECT * FROM kind_type");
+//                            resultSet2.next();
+//
+//                            row.put("kind", resultSet2.getString(2));
+//
+//                            resultSet2.close();
+//                            statementSQL2.close();
+//                        }
+//                    }
+//                }
+//                //Insert keyword
+//               // row.put("keyword", "");
 
-                            row.put("kind", resultSet2.getString(2));
-
-                            resultSet2.close();
-                            statementSQL2.close();
-                        }
-                    }
-                }
-                //Insert keyword
-               // row.put("keyword", "");
-
-                System.out.println(dem++ + " : ++++" + row);
-                tableAPI.put(row, null, null);
+//                System.out.println(dem++ + " : ++++" + row);
+//                tableAPI.put(row, null, null);
             }
 
             resultSet.close();
@@ -108,5 +107,118 @@ public class Migrate {
         }
 
         return list;
+    }
+
+    private void insertInto_complete_cast(int id_movie,TableAPI tableAPI) {
+        try {
+            Table table = tableAPI.getTable("title.complete_cast");
+            Row row = table.createRow();
+
+            Statement statementSQL2 = connectSQL.createStatement();
+            ResultSet resultSet2 = statementSQL2.executeQuery(
+                    "SELECT status_id, kind as subject FROM imdb.complete_cast " +
+                    "JOIN comp_cast_type ON comp_cast_type.id = subject_id " +
+                    "WHERE movie_id = " + id_movie);
+            int i = 1;
+            Statement statementSQL3 = connectSQL.createStatement();
+            ResultSet resultSet3 = null;
+            while (resultSet2.next()) {
+                 resultSet3 = statementSQL3.executeQuery(
+                        "SELECT kind FROM imdb.comp_cast_type WHERE id =" + resultSet2.getInt(1));
+                resultSet3.next();
+                row.put("id", id_movie);
+                row.put("id_cast", i++);
+                row.put("subject", resultSet2.getString(2));
+                row.put("status", resultSet3.getString(1));
+
+                tableAPI.put(row, null,null);
+                System.out.println(row);
+            }
+
+            resultSet2.close();
+            statementSQL2.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertInto_company(int id_movie,TableAPI tableAPI) {
+        try {
+            Table table = tableAPI.getTable("title");
+            Row row = table.createRow();
+
+            Statement statementSQL2 = connectSQL.createStatement();
+            ResultSet resultSet2 = statementSQL2.executeQuery(
+                    "SELECT keyword FROM movie_keyword JOIN keyword ON keyword.id = movie_keyword.keyword_id WHERE movie_id =" + id_movie);
+
+            while (resultSet2.next()) {
+
+            }
+
+            resultSet2.close();
+            statementSQL2.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertInto_infor_movie(int id_movie,TableAPI tableAPI) {
+        try {
+            Table table = tableAPI.getTable("title");
+            Row row = table.createRow();
+
+            Statement statementSQL2 = connectSQL.createStatement();
+            ResultSet resultSet2 = statementSQL2.executeQuery(
+                    "SELECT keyword FROM movie_keyword JOIN keyword ON keyword.id = movie_keyword.keyword_id WHERE movie_id =" + id_movie);
+
+            while (resultSet2.next()) {
+
+            }
+
+            resultSet2.close();
+            statementSQL2.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertInto_aka_title(int id_movie,TableAPI tableAPI) {
+        try {
+            Table table = tableAPI.getTable("title");
+            Row row = table.createRow();
+
+            Statement statementSQL2 = connectSQL.createStatement();
+            ResultSet resultSet2 = statementSQL2.executeQuery(
+                    "SELECT keyword FROM movie_keyword JOIN keyword ON keyword.id = movie_keyword.keyword_id WHERE movie_id =" + id_movie);
+
+            while (resultSet2.next()) {
+
+            }
+
+            resultSet2.close();
+            statementSQL2.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void insertInto_cast_info(int id_movie,TableAPI tableAPI) {
+        try {
+            Table table = tableAPI.getTable("title");
+            Row row = table.createRow();
+
+            Statement statementSQL2 = connectSQL.createStatement();
+            ResultSet resultSet2 = statementSQL2.executeQuery(
+                    "SELECT keyword FROM movie_keyword JOIN keyword ON keyword.id = movie_keyword.keyword_id WHERE movie_id =" + id_movie);
+
+            while (resultSet2.next()) {
+
+            }
+
+            resultSet2.close();
+            statementSQL2.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
